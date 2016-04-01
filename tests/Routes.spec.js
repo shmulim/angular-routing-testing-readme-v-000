b5f44079ea21f7bba17082ba465bc522ad4d45b3
shmulim <shmuel.markel@gmail.com>
2016-04-01 13:41:33 -0700

    Done.

--- tests/Routes.spec.js
+++ tests/Routes.spec.js
@@ -7,13 +7,50 @@ describe('Routes', function () {
 		$state = $injector.get('$state');
 	}));
 
-	describe('Settings', function () {
+	describe('settings state', function () {
 		var state;
 
-		it('should have the correct URL', function () {
+		it('should have the correct settings URL', function () {
 			state = $state.get('settings');
 
 			expect(state.url).toEqual('/settings');
 		});
+
+		it('should have the correct settings template', function(){
+			expect(state.templateUrl).toEqual('views/settings.html');
+		});
+
 	});
+
+		describe('settings.user state', function () {
+		var state;
+
+		it('should have the correct settings URL', function () {
+			state = $state.get('settings.user');
+
+			expect(state.url).toEqual('/user');
+		});
+
+		it('should have the correct settings template', function(){
+			expect(state.templateUrl).toEqual('views/settings/user.html');
+		});
+
+	});
+
+
+	describe('settings.notifications state', function () {
+		var state;
+
+		it('should have the correct settings URL', function () {
+			state = $state.get('settings.notifications');
+
+			expect(state.url).toEqual('/notifications');
+		});
+
+		it('should have the correct settings template', function(){
+			expect(state.templateUrl).toEqual('views/settings/notifications.html');
+		});
+
+	});
+
 });
